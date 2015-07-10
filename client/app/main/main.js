048dd02528672f8ba1537ea4d01aa06b075f9490
Paul-Guillaume Déjardin <paul-guillaume.dejardin@ei-technologies.com>
2015-07-10 13:38:37 +0200

    Initial commit

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('frontEndExerciseApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
