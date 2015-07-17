e566f2675f0c500b046a433a656f559df46f57dd
Paul-Guillaume Déjardin <paul-guillaume.dejardin@ei-technologies.com>
2015-07-17 16:57:14 +0200

    Calcul de la meilleure reduction sur le panier

--- client/app/cart/cart.controller.js
+++ client/app/cart/cart.controller.js
@@ -1,12 +1,45 @@
 'use strict';
 
 angular.module('frontEndExerciseApp')
-  .controller('CartCtrl', function($scope, ngCart) {
-    $scope.message = 'Hello';
+  .controller('CartCtrl', function($scope, ngCart, booksService) {
+
+    $scope.$on('ngCart:change', function() {
+      getPromotions();
+    });
+
+    function calculateReduction(offers) {
+      var reductions = [];
+      _.forEach(offers.offers, function(offer) {
+        switch (offer.type) {
+          case 'percentage':
+            reductions.push(ngCart.getSubTotal() * offer.value / 100);
+            break;
+          case 'minus':
+            reductions.push(offer.value);
+            break;
+          case 'slice':
+            reductions.push((ngCart.getSubTotal() / offer.sliceValue >> 0) * offer.value); // jshint ignore:line
+            break;
+        }
+      });
+      return reductions;
+    }
+
+    function getPromotions() {
+      var isbnList = _.pluck(ngCart.getItems(), '_id');
+      if (!_.isEmpty(isbnList)) {
+        booksService.getPromotions(isbnList).success(function(res) {
+          var reductions = calculateReduction(res);
+          ngCart.setShipping(-Math.max.apply(Math, reductions));
+        }).error(function(err) {
+          console.error(err);
+        });
+      }
+    }
 
     var init = function() {
       ngCart.setTaxRate(0);
-      ngCart.setShipping(-15.0);
+      getPromotions();
     };
 
     init();
--- client/components/services/books/books.service.js
+++ client/components/services/books/books.service.js
@@ -4,11 +4,14 @@ angular.module('frontEndExerciseApp')
   .service('booksService', function($http) {
     // AngularJS will instantiate a singleton by calling "new" on this function
 
-    var proxyURL = '/api/proxy';
+    var proxyURL = '/api/proxy/books';
 
     return {
       getBooks: function() {
-        return $http.get(proxyURL + '/books');
+        return $http.get(proxyURL + '/');
+      },
+      getPromotions: function(isbnList) {
+        return $http.get(proxyURL + '/' + isbnList + '/commercialOffers');
       }
     };
 
